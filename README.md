da8e38c38fc4feccf75edc6106df0e705e5fb7a6
Guilherme Ramos <guilherme.silvano@hotmail.com>
2023-08-29 20:06:04 -0300

    scroll app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# flutter-scroll-animate
-Controller scroll with animation Tab Bar.
+# Flutte Scroll Animated
+
+![Exemplo](demo_opt.gif)
+
+In this example, we'll simulate the list of dishes, food items, or products within an establishment, taking inspiration from the iFood app. Our focus is on enabling programmatic scrolling that allows users to navigate to categories initially not visible on the screen.
+
+Moreover, as users scroll through the list, we'll implement a listener that automatically changes the active tab. This will provide a more dynamic and intuitive experience for users. We aim to demonstrate how to create this feature to enhance usability and interaction with the item list in the app.
+
---
+++ lib/pages/restaurante_detalhes.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_controle_scroll/models/comida.dart';
+import 'package:flutter_controle_scroll/repositories/comida_repository.dart';
+
+class RestauranteDetalhes extends StatefulWidget {
+  RestauranteDetalhes({Key? key}) : super(key: key);
+
+  @override
+  _RestauranteDetalhesState createState() => _RestauranteDetalhesState();
+}
+
+class _RestauranteDetalhesState extends State<RestauranteDetalhes> {
+  final List<GlobalKey> categorias = [GlobalKey(), GlobalKey(), GlobalKey()];
+  late ScrollController scrollCont;
+  BuildContext? tabContext;
+
+  @override
+  void initState() {
+    scrollCont = ScrollController();
+    scrollCont.addListener(changeTabs);
+    super.initState();
+  }
+
+  changeTabs() {
+    late RenderBox box;
+
+    for (var i = 0; i < categorias.length; i++) {
+      box = categorias[i].currentContext!.findRenderObject() as RenderBox;
+      Offset position = box.localToGlobal(Offset.zero);
+
+      if (scrollCont.offset >= position.dy)
+        DefaultTabController.of(tabContext!)!.animateTo(
+          i,
+          duration: Duration(milliseconds: 100),
+        );
+    }
+  }
+
+  scrollTo(int index) async {
+    scrollCont.removeListener(changeTabs);
+    final categoria = categorias[index].currentContext!;
+    await Scrollable.ensureVisible(
+      categoria,
+      duration: Duration(milliseconds: 600),
+    );
+    scrollCont.addListener(changeTabs);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Builder(
+        builder: (BuildContext context) {
+          tabContext = context;
+          return Scaffold(
+            appBar: AppBar(
+              leading: BackButton(color: Colors.red),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.red,
+                  ),
+                )
+              ],
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'BURGÃO',
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    'Entrega em 40-50 min',
+                    style: TextStyle(fontSize: 12, color: Colors.black54),
+                  )
+                ],
+              ),
+              bottom: TabBar(
+                tabs: [
+                  Tab(child: Text('Carnes')),
+                  Tab(child: Text('Burgers')),
+                  Tab(child: Text('Combos')),
+                ],
+                onTap: (int index) => scrollTo(index),
+              ),
+            ),
+            body: SingleChildScrollView(
+              controller: scrollCont,
+              child: Column(
+                children: [
+                  categoriaComida('Carnes', 0),
+                  gerarListaComidas(ComidaRepository.carnes),
+                  categoriaComida('Burgers', 1),
+                  gerarListaComidas(ComidaRepository.burgers),
+                  categoriaComida('Combos', 2),
+                  gerarListaComidas(ComidaRepository.combos),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  gerarListaComidas(List<Comida> comidas) {
+    return Column(
+      children: comidas.map((comida) => comidaItem(comida)).toList(),
+    );
+  }
+
+  Widget comidaItem(Comida comida) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(comida.nome),
+          subtitle: Padding(
+            padding: EdgeInsets.only(top: 12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  comida.descricao,
+                  style: TextStyle(fontSize: 13),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 24),
+                  child: Text(
+                    comida.preco,
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          trailing: Image.network(comida.imagem),
+          contentPadding: EdgeInsets.all(15),
+        ),
+        Divider(),
+      ],
+    );
+  }
+
+  Widget categoriaComida(String titulo, int index) {
+    return Padding(
+      key: categorias[index],
+      padding: EdgeInsets.only(top: 24),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              titulo,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
